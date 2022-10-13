d26e7fc1bfc86e21a11f32a9f005506af55e5bca
Jason King <jason.king@conversocial.com>
2022-10-13 11:56:44 +0100

    Initial implementation of roman numeral converter

--- src/main/java/com/conversocial/katas/RomanNumerals.java
+++ src/main/java/com/conversocial/katas/RomanNumerals.java
@@ -1,8 +1,27 @@
 package com.conversocial.katas;
 
+/**
+ * Helper for handling roman numerals
+ * http://www.novaroma.org/via_romana/numbers.html
+ */
 public class RomanNumerals {
 
-    public static Object convert(int i) {
-        return "I";
+    /**
+     * This method is converting a number into Roman Numeral equivalent.
+     * @param number the number to convert
+     * @return Roman Numeral equivalent
+     * @throws IllegalArgumentException if number is unsupported
+     */
+    public static String convert(int number) {
+        if (number==1) {
+            return "I";
+        } else if (number==7) {
+            return "VII";
+        } else if (number==10) {
+            return "X";
+        } else {
+            throw new IllegalArgumentException("This number is not supported in Romans: "+number);
+        }
     }
+
 }
--- src/test/java/com/conversocial/katas/RomanNumeralsTest.java
+++ src/test/java/com/conversocial/katas/RomanNumeralsTest.java
@@ -7,8 +7,23 @@ import static org.junit.Assert.assertEquals;
 public class RomanNumeralsTest {
 
     @Test
-    public void test1() {
+    public void testI() {
         assertEquals("I", RomanNumerals.convert(1) );
+    }
+
+    @Test
+    public void testVII() {
+        assertEquals("VII", RomanNumerals.convert(7) );
+    }
+
+    @Test
+    public void testX() {
+        assertEquals("X", RomanNumerals.convert(10) );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUnknownNumber() {
+        RomanNumerals.convert(0);
     }
 
 }
